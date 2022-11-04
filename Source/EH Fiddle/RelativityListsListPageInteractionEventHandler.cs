d032a780ea76f1891e4624e68958502c29be04d0
ObviouslyWorking <35307977+obviouslyworking@users.noreply.github.com>
2022-11-04 09:54:25 -0500

    Added relativity.lists extension for eh fiddle
    Force-redirects away from EH Fiddle list page to creation Form (allows direct access to EH Fiddle via tab).

---
+++ Source/EH Fiddle/RelativityListsListPageInteractionEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using kCura.EventHandler;
+using Relativity.API;
+using Relativity.Services.Objects;
+using System;
+using System.Net;
+
+namespace EH_Fiddle
+{
+	[kCura.EventHandler.CustomAttributes.Description("List Page Interaction EventHandler")]
+	[System.Runtime.InteropServices.Guid("274a55ca-d37f-4bd8-ba91-950dd13fc633")]
+	public class RelativityListsListPageInteractionEventHandler : kCura.EventHandler.ListPageInteractionEventHandler
+	{
+		public override Response PopulateScriptBlocks()
+		{
+			// Create an empty response object with default values
+			// Purely for syntax correctness of the class -- Relativity.Lists ignores this function
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+			kCura.EventHandler.Response retVal = new kCura.EventHandler.Response();
+			retVal.Success = true;
+			retVal.Message = string.Empty;
+			return retVal;
+		}
+
+		public override string[] ScriptFileNames => new string[] { "lists.ext.ehfiddle.js" };
+
+		public override string[] AdditionalHostedFileNames => new string[] { };
+	}
+}
---
+++ Source/EH Fiddle/lists.ext.ehfiddle.js
@@ -0,0 +1,23 @@
+﻿function cerr(...args) {
+	console.error("EH FIDDLE Relativity.Lists extension error", ...args);
+}
+
+if (convenienceApi) {
+	const atid = Number.parseInt(convenienceApi.artifactTypeID, 10);
+	if (!isNaN(atid)) {
+		if (atid > 0) {
+			if (window.top.relativity && window.top.relativity.navigation && (typeof window.top.relativity.navigation.navigate === "function")) {
+				// tell Relativity to navigate immediately away from the EH Fiddle list page to the creation form
+				window.top.relativity.navigation.navigate(`/Relativity/RelativityInternal.aspx?Mode=Forms&FormMode=add&AppID=${convenienceApi.workspaceID}&ArtifactTypeID=${atid}`);
+			} else {
+				cerr("Could not find the Relativity window via window.top");
+			}
+		} else {
+			cerr("The convenienceApi.artifactTypeID less than 1, which is invalid.");
+		}
+	} else {
+		cerr("The convenienceApi.artifactTypeID is NaN.");
+	}
+} else {
+	cerr("The convenienceApi is falsy.");
+}
